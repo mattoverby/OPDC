fb3388a036447669bb59a88d74815f97f2eed85b
mattoverby <mattoverby@gmail.com>
2019-02-13 21:38:34 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# OPDC
+Unofficial repository for the One Page Dungeon Contest
+
+[www.dungeoncontest.com](https://www.dungeoncontest.com)
+
+## A note on this collection
+
+For better archiving and scripting, I've changed the file names and types to be more consistent. If you see an error, please submit an issue.
+
+## A note on the license
+
+Submissions to OPDC require the [CC BY-SA 3.0 license](https://creativecommons.org/licenses/by-sa/3.0). However, older submissions may have had different requirements.
+Please review the individual text to see the exact license. Otherwise, submit an issue and I'll add a specific note.
+
+## To-do
+
+- Verify I have all the entries and they aren't corrupted
+- A catalog.json that stores the file name and properly formatted author names and titles.
+- READMEs in each folder with nice links and categories (winners, etc...) created from above catalog.json.
+- Latex script to create printable book using above catalog.json.
---
+++ scripts/loop_files.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+import os
+import subprocess
+
+def list_nonpdf():
+	years = ['OPDC2009','OPDC2010','OPDC2011','OPDC2012','OPDC2013','OPDC2014','OPDC2015','OPDC2016','OPDC2017','OPDC2018'];
+	for year in years:
+		directory = "../" + year + "/";
+		for fn in os.listdir(directory):
+#			if fn[-4:] != '.pdf':
+#			if fn[-4:] == '.tif':
+				print "\n" + directory + fn;
+#				print directory + fn[:-4] + ".pdf";
+#				os.system("img2pdf -o "+directory+fn[:-4]+".pdf "+directory+fn)
+#				os.system("convert "+directory+fn+" -quality 100 "+directory+fn[:-4]+".pdf")
+#				subprocess.call(["git", "add", directory+fn[:-4]+".pdf"])
+#				subprocess.call(["git", "rm", directory+fn])
+
+#def strip_nonalphanumeric(astring):
+#	astring = astring.replace("_", "");
+#	astring = astring.replace("-", "");
+#	astring = astring.replace(",", "");
+#	astring = astring.replace("(", "");
+#	astring = astring.replace(")", "");
+#	astring = astring.replace("&", "");
+#	astring = astring.replace("!", "");
+#	astring = astring.replace("\"", "");
+#	return astring;
+
+#def fix_2010():
+#	directory = "../OPDC2011/"
+#	sid = 1;
+#	for fn in os.listdir(directory):
+#		if sid < 10: 
+#			sids = "E00" + str(sid);
+#		elif sid < 100: 
+#			sids = "E0" + str(sid);
+#		elif sid < 1000: 
+#			sids = "E" + str(sid);
+	#	if "__" not in fn: 
+	#		sid += 1;
+	#		continue
+#		name_title_idx = ;
+#		name = fn[0:name_title_idx];
+#		title = fn[name_title_idx+2:];
+#		name = strip_nonalphanumeric(name);
+#		name = name.replace(".", "");
+#		title = strip_nonalphanumeric(title);
+#		title = title.replace("...", "");
+#		newfn = directory + sids + "_" + name + "_" + title;
+#		oldfn = directory + fn;
+#		print newfn;
+#		subprocess.call(["git", "mv", oldfn, newfn])
+#		sid += 1;
+
+#def fix_2017():
+#	directory = "../" + "OPDC2017/"
+#	for fn in os.listdir(directory):
+#		newfn = directory + "E0" + fn[0] + fn[1] + "_" + fn[3].upper() + fn[4:];
+#		oldfn = directory + fn;
+#		subprocess.call(["git", "mv", oldfn, newfn])
+
+#def fix_2018():
+#	directory = "../" + "OPDC2018/"
+#	for fn in os.listdir(directory):
+#		if fn[3] == "_":
+#			newfn = directory + "E0" + fn[1] + fn[2] + "_" + fn[4:];
+#			oldfn = directory + fn;
+#			subprocess.call(["git", "mv", oldfn, newfn])
+
+if __name__ == '__main__':
+	list_nonpdf()
