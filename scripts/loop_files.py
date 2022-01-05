0f57274158c9456a0c0fb71617a5996995083e9e
mattoverby <mattoverby@gmail.com>
2022-01-04 19:45:35 -0600

    added 2020

--- scripts/loop_files.py
+++ scripts/loop_files.py
@@ -1,87 +1,93 @@
-#!/usr/bin/python3
+#!/usr/bin/env python
+
+import sys
 import os
-import subprocess
+#from pathlib import Path
 
-def list_nonpdf():
-	years = ['OPDC2009','OPDC2010','OPDC2011','OPDC2012','OPDC2013','OPDC2014','OPDC2015','OPDC2016','OPDC2017','OPDC2018'];
-	for year in years:
-		directory = "../" + year + "/";
-		for fn in os.listdir(directory):
-			if fn[-4:] != '.pdf':
-#			if fn[-4:] == '.tif':
-				print "\n" + directory + fn;
-#				print directory + fn[:-4] + ".pdf";
-#				os.system("img2pdf -o "+directory+fn[:-4]+".pdf "+directory+fn)
-#				os.system("convert "+directory+fn+" -quality 100 "+directory+fn[:-4]+".pdf")
-#				subprocess.call(["git", "add", directory+fn[:-4]+".pdf"])
-#				subprocess.call(["git", "rm", directory+fn])
+def check_input():
+    if len(sys.argv) < 2:
+        print("Usage: ./loop_files.py <directory>")
+        sys.exit()
+# end check input
 
-def strip_nonalphanumeric(astring):
-#	astring = astring.replace("_", "");
-	astring = astring.replace("-", "");
-	astring = astring.replace(",", "");
-	astring = astring.replace("(", "");
-	astring = astring.replace(")", "");
-	astring = astring.replace("&", "");
-	astring = astring.replace("!", "");
-	astring = astring.replace("\"", "");
-	astring = astring.replace(" ", "");
-	return astring;
+def gather_files(a_dir):
+    return [name for name in os.listdir(a_dir)
+        if os.path.isfile(os.path.join(a_dir, name))]
+# end gather tests
 
-#def fix_2010():
-#	directory = "../OPDC2011/"
-#	sid = 1;
-#	for fn in os.listdir(directory):
-#		if sid < 10: 
-#			sids = "E00" + str(sid);
-#		elif sid < 100: 
-#			sids = "E0" + str(sid);
-#		elif sid < 1000: 
-#			sids = "E" + str(sid);
-	#	if "__" not in fn: 
-	#		sid += 1;
-	#		continue
-#		name_title_idx = ;
-#		name = fn[0:name_title_idx];
-#		title = fn[name_title_idx+2:];
-#		name = strip_nonalphanumeric(name);
-#		name = name.replace(".", "");
-#		title = strip_nonalphanumeric(title);
-#		title = title.replace("...", "");
-#		newfn = directory + sids + "_" + name + "_" + title;
-#		oldfn = directory + fn;
-#		print newfn;
-#		subprocess.call(["git", "mv", oldfn, newfn])
-#		sid += 1;
+# Convert to pdf if it isn't one
+def print_not_pdf(filename):
+    ext = os.path.splitext(filename)[1]
+    if ext != '.pdf':
+        print(filename)
+# end make pdf
 
-#def fix_2017():
-#	directory = "../" + "OPDC2017/"
-#	for fn in os.listdir(directory):
-#		newfn = directory + "E0" + fn[0] + fn[1] + "_" + fn[3].upper() + fn[4:];
-#		oldfn = directory + fn;
-#		subprocess.call(["git", "mv", oldfn, newfn])
+def strip_nonalphanumeric(f_dir, filename):
+    astring = filename
+    #astring = astring.replace("_", ""_
+    astring = astring.replace("-", "")
+    astring = astring.replace(",", "")
+    astring = astring.replace("(", "")
+    astring = astring.replace(")", "")
+    astring = astring.replace("&", "")
+    astring = astring.replace("!", "")
+    astring = astring.replace("\"", "")
+    astring = astring.replace(" ", "")
+    print(astring)
+    #os.rename(f_dir+filename, f_dir+astring)
+# end strip alpha numeric
 
-#def fix_2018():
-#	directory = "../" + "OPDC2018/"
-#	for fn in os.listdir(directory):
-#		if fn[3] == "_":
-#			newfn = directory + "E0" + fn[1] + fn[2] + "_" + fn[4:];
-#			oldfn = directory + fn;
-#			subprocess.call(["git", "mv", oldfn, newfn])
+def remove_v2(f_dir, filename):
+    if filename.count('_') == 2:
+        return
+    strs = filename.split('_')
+    if strs[3] != 'v2.pdf':
+        return
+    print(filename)
+    new_fn = strs[0]+'_'+strs[1]+'_'+strs[2]+'.pdf'
+    #os.rename(f_dir+filename, f_dir+new_fn)
+    print(new_fn)
+# end remove v2
 
-def fix_2019():
-	directory = "../" + "OPDC2019/"
-	for fn in os.listdir(directory):
-		if fn[4] == "_":
-			#newfn = directory + "E0" + fn[1] + fn[2] + "_" + fn[4:];
-			newfn = directory + fn;
-			newfn = strip_nonalphanumeric(newfn);
-			print newfn;
-			oldfn = directory + fn;
-			#print oldfn;
-			subprocess.call(["mv", oldfn, newfn])
-			#subprocess.call(["git", "mv", oldfn, newfn])
+def add_leading_zeros_2020(f_dir, filename):
+    # format: NUM_NAME_TITLE.PDF
+    new_fn = filename
+    strs = new_fn.split('_')
+    if strs[0][0] == 'E': # already done
+        return
+    len_0 = len(strs[0])
+    assert (len_0==2 or len_0==3)
+    if len_0<3:
+        new_fn = '0' + new_fn
+        
+    new_fn = 'E' + new_fn
+    print(new_fn)
+    #os.rename(f_dir+filename, f_dir+new_fn)
+# end add leading zeros
 
-if __name__ == '__main__':
-#	fix_2019()
-	list_nonpdf()
+#
+# Main
+#
+def main():
+
+    check_input()
+
+    # file directory
+    f_dir = sys.argv[1]
+    if f_dir[-1] != '/':
+        f_dir += '/'
+
+    files = gather_files(f_dir)
+    files.sort()
+
+    for filename in files:
+        #strip_nonalphanumeric(f_dir, filename)
+        if filename.count('_') != 2:
+        #if filename.count('.') > 1:
+            print(filename)
+    # end loop files
+    
+# end main
+
+if __name__=="__main__":
+    main()
